e3e8ee477c4c0bd45b93cb9596d4898f3b9ae793
Theo <theobbb64@gmail.com>
2025-11-23 12:34:37 -0500

    removed .env completely

--- src/lib/supabase-client.ts
+++
@@ -1,4 +0,0 @@
-import { PUBLIC_SUPABASE_PUBLISHABLE_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
-import { createClient } from '@supabase/supabase-js';
-
-export const supabase_client = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_PUBLISHABLE_KEY);
